db77b0715627451c2bb377150eeb04470009f2fc
DoebelnAtz <axel.ej.adlercreutz@gmail.com>
2024-12-02 22:01:09 +0200

    dark mode

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template, session, url_for
 from app.utils.auth import login_required
 from app.utils.errors import handle_errors
 from app.utils.groups import get_user_groups, get_user_invites
@@ -14,3 +14,11 @@ def index():
     user_groups = get_user_groups()
     user_invites = get_user_invites()
     return render_template('index.html', groups=user_groups, invites=user_invites)
+
+
+@root_bp.route('/toggle_theme', methods=['POST'])
+@login_required
+@handle_errors
+def toggle_theme():
+    session['theme'] = 'dark' if session.get('theme') == 'light' else 'light'
+    return redirect(url_for('root.index'))
