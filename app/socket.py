cc3ee4510daa4ae50f1111dab7d5d63452afe1a3
DoebelnAtz <axel.ej.adlercreutz@gmail.com>
2024-12-14 12:30:27 +0200

    added localhosts to cors

--- app/socket.py
+++ app/socket.py
@@ -5,7 +5,7 @@ from app.utils.errors import handle_socket_errors
 from app.utils.logger import Logger
 
 logger = Logger('Socket')
-socketio = SocketIO(cors_allowed_origins=['http://localhost:5000', 'https://taival.app'])
+socketio = SocketIO(cors_allowed_origins=['http://127.0.0.1:5000', 'http://localhost:5000', 'https://taival.app'])
 
 
 def register_socket_events():
